2968fa5d606195f9a01087b5d25e0e4a5f4b1bad
unknown <jeremie229242@gmail.com>
2025-01-24 16:42:44 +0100

    premiere etape 04

--- portfolio1/index.js
+++ portfolio1/index.js
@@ -1,21 +1,30 @@
 /*========== menu icon navbar ==========*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
 
 
-/*========== scroll sections active link ==========*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+// /*========== scroll sections active link ==========*/
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+
 
 window.onscroll = () => {
+    let header = document.querySelector('.header');
+
+    header.classList.toggle('sticky', window.scrollY > 180);
+    
+
+}
+// window.onscroll = () => {
 
 
-    /*========== sticky navbar ==========*/
+//     /*========== sticky navbar ==========*/
 
 
-    /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
 
-};
+//     /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+
+// };
 
 
 /*========== swiper ==========*/
