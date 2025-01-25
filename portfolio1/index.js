a0988d46b705169d1a23abb7d579441e33417707
unknown <jeremie229242@gmail.com>
2025-01-25 11:57:57 +0100

    premiere etape 09

--- portfolio1/index.js
+++ portfolio1/index.js
@@ -12,6 +12,8 @@ let menuIcon = document.querySelector('#menu-icon');
  window.onscroll = () => {
 
 
+
+
      /*========== sticky navbar ==========*/
      let header = document.querySelector('.header');
 
@@ -19,7 +21,19 @@ let menuIcon = document.querySelector('#menu-icon');
 
 
      /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
+    if(top >= offset && top < offset + height) {
+navLinks.forEach(links =>{
+    links.classList.remove('active');
+    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+})
+    }
+})
 };
 
 
